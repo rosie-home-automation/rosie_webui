faad4cc161bcbee676954312948d84f2072db6a0
olson3r <scootles.olson@gmail.com>
2015-09-26 14:15:22 -0500

    Add remotes menu item

--- assets/ng/remotes/remotes_module.js
+++ assets/ng/remotes/remotes_module.js
@@ -1,5 +1,56 @@
 angular.module('rosieApp.remotes', [
   'ui.router',
   'ui.bootstrap',
-  'rosieApp.config'
+  'rosieApp.config',
+  'rosieApp.header'
 ])
+  .config(['$stateProvider', 'MenuProvider', function($stateProvider, menuProvider) {
+    $stateProvider
+      .state('app.remote', {
+        abstract: true,
+        url: '/remotes',
+        templateUrl: 'ng/remotes/remote.tmpl.html',
+        controller: 'RemoteController as remoteCtrl'
+      })
+      .state('app.remote.receiver', {
+        url: '/receiver',
+        views: {
+          '': {
+            templateUrl: 'ng/remotes/remote_receiver.tmpl.html',
+            controller: 'RemoteReceiverController as remoteReceiverCtrl'
+          },
+          'breadcrumb': {
+            template: 'Receiver'
+          }
+        }
+      })
+      .state('app.remote.tv', {
+        url: '/tv',
+        views: {
+          '': {
+            templateUrl: 'ng/remotes/remote_tv.tmpl.html',
+            controller: 'RemoteTvController as remoteTvCtrl'
+          },
+          'breadcrumb': {
+            template: 'TV'
+          }
+        }
+      })
+      .state('app.remote.ps3', {
+        url: '/ps3',
+        views: {
+          '': {
+            templateUrl: 'ng/remotes/remote_ps3.tmpl.html',
+            controller: 'RemotePs3Controller as remotePs3Ctrl'
+          },
+          'breadcrumb': {
+            template: 'PS3'
+          }
+        }
+      })
+
+    menuProvider.add({
+      title: 'Remotes',
+      sref: 'app.remotes'
+    })
+  }])
